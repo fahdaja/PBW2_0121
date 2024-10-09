2ae4752741b9c007b0470b88cdccb411629b821f
fahdaja <fahdalbantani02@gmail.com>
2024-10-09 17:27:51 +0700

    mengerjakan modul 4

--- resources/views/buku/index.blade.php
+++ resources/views/buku/index.blade.php
@@ -1,5 +1,48 @@
 <x-layout>
-    <h1>Ini Halaman Buku</h1>
+    <div class="container mt-5">
+        <div class="card">
+            <h3>Ini Halaman Buku</h3>
+            <div class="card-body">
+                <div class="col-sm-7">
+                    <a href="/buku" class="btn btn-primary">Tambah Buku</a>
+                </div>
+                <table class="table table-bordered table-striped table-hover " id="myTable">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Judul</th>
+                            <th>Penulis</th>
+                            <th>Kategori</th>
+                            <th>Sampul</th>
+                            <th>Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    @foreach ($bukus as $buku)
+                    <tr class="text-center">
+                        <td class="text-center">
+                        <td>{{ $buku->judul }}</td>
+                        <td>{{ $buku->penulis }}</td>
+                        <td>{{ $buku->kategori }}</td>
+                                <td>
+                                    <img src="storage/{{ $buku->sampul }}" class="rounded img-fluid" width="100px">
+                                </td>
+                                <td>
+                                    <a class="btn btn-warning" href="/buku/{{ $buku->id }}/edit">Ubah</a>
+                                    <form action="/buku/{{ $buku->id }}" method="post" class="d-inline ms-1">
+                                        @method('delete')
+                                        @csrf
+                                        <button class="btn btn-danger" 
+                                        onclick="return confirm('Are you sure?')">Hapus</button>
+                                    </form>
+                                </td>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 
-    <a href="buku/create">tambah data</a>
 </x-layout>
